c0093106a3b2ede29334d654008d6f95d4f1d207
Gui Gutierrez <guillermogut@umass.edu>
2022-03-28 14:50:58 -0400

    remade build

--- back/server.js
+++ back/server.js
@@ -13,9 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const publicPath = path.join(__dirname,  'build');
-
-
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server listening on ${PORT}`);
 });
